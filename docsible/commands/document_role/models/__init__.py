2957755b78073e34f5dbe5562843b6475aa15227
Jier <jiernzuanzu@gmail.com>
2025-12-22 03:28:16 +0100

    refactor: core.py work in progress

---
+++ docsible/commands/document_role/models/__init__.py
@@ -0,0 +1,25 @@
+"""Models for document_role command."""
+
+from .role_command_context import (
+    AnalysisConfig,
+    ContentFlags,
+    DiagramConfig,
+    PathConfig,
+    ProcessingConfig,
+    RepositoryConfig,
+    RoleCommandContext,
+    TemplateConfig,
+    ValidationConfig,
+)
+
+__all__ = [
+    "RoleCommandContext",
+    "PathConfig",
+    "TemplateConfig",
+    "ContentFlags",
+    "DiagramConfig",
+    "AnalysisConfig",
+    "ProcessingConfig",
+    "ValidationConfig",
+    "RepositoryConfig",
+]
